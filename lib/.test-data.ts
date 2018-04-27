7792d25eef6f2e23b4f9a6ee43e13ea819471933
Jason Abbott <dev@trailimage.com>
2018-04-27 10:42:15 -0600

    require envs

--- lib/.test-data.ts
+++ lib/.test-data.ts
@@ -1,3 +1,4 @@
+import { env } from '@toba/tools';
 import { GoogleConfig } from '@toba/google-drive';
 import { Config as AuthConfig } from '@toba/oauth';
 import { Post, blog } from '@trailimage/models';
@@ -6,19 +7,19 @@ import { Post, blog } from '@trailimage/models';
  * @see http://code.google.com/apis/console/#project:1033232213688
  */
 export const testConfig: GoogleConfig = {
-   apiKey: process.env['GOOGLE_DRIVE_KEY'],
+   apiKey: env('GOOGLE_DRIVE_KEY'),
    folderID: '0B0lgcM9JCuSbMWluNjE4LVJtZWM',
    useCache: false,
    cacheSize: 0,
    auth: {
-      clientID: process.env['GOOGLE_CLIENT_ID'],
-      secret: process.env['GOOGLE_SECRET'],
+      clientID: env('GOOGLE_CLIENT_ID'),
+      secret: env('GOOGLE_SECRET'),
       callback: 'http://localhost/auth/google',
       token: {
          type: null,
-         access: process.env['GOOGLE_ACCESS_TOKEN'],
+         access: env('GOOGLE_ACCESS_TOKEN'),
          accessExpiration: null as Date,
-         refresh: process.env['GOOGLE_REFRESH_TOKEN']
+         refresh: env('GOOGLE_REFRESH_TOKEN', null)
       }
    } as AuthConfig
 };
