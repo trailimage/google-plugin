c6e4b408ecd30f687d2a22249516f191264f0fc2
Jason Abbott <dev@trailimage.com>
2018-06-17 13:28:27 -0600

    use unlist method

--- lib/provider.ts
+++ lib/provider.ts
@@ -1,13 +1,29 @@
 import { MapProvider } from '@trailimage/models';
+import { unlist } from '@toba/tools';
+import { IncomingMessage } from 'http';
+import { parse } from 'url';
 import { GoogleConfig } from '@toba/google-drive';
 import { loadTrack } from './track';
+import { googleDrive } from './client';
 
 export interface ProviderConfig {
    api: GoogleConfig;
 }
 
 class GoogleProvider extends MapProvider<ProviderConfig> {
-   track = (postKey: string) => loadTrack(postKey);
+   track(postKey: string) {
+      return loadTrack(postKey);
+   }
+
+   authorizationURL() {
+      return Promise.resolve(googleDrive.client.authorizationURL);
+   }
+
+   async getAccessToken(req: IncomingMessage) {
+      const url = parse(req.url, true);
+      const code = unlist(url.query['code'], true);
+      return googleDrive.client.getAccessToken(code);
+   }
 }
 
 export const provider = new GoogleProvider({ api: null });
