c06f3b3c84568bcc43b40d8dfca2fa968d17bb37
Jason Abbott <dev@trailimage.com>
2019-10-03 16:46:01 -0600

    implement clearCache no-op

--- src/provider.ts
+++ src/provider.ts
@@ -1,38 +1,42 @@
-import { MapProvider } from '@trailimage/models';
-import { MapConfig } from '@toba/map';
-import { unlist } from '@toba/node-tools';
-import { IncomingMessage } from 'http';
-import { parse } from 'url';
-import { GoogleConfig } from '@toba/google-drive';
-import { loadTrack, streamGPX } from './track';
-import { googleDrive } from './client';
-import { Writable } from 'stream';
+import { MapProvider } from "@trailimage/models";
+import { MapConfig } from "@toba/map";
+import { unlist } from "@toba/node-tools";
+import { IncomingMessage } from "http";
+import { parse } from "url";
+import { GoogleConfig } from "@toba/google-drive";
+import { loadTrack, streamGPX } from "./track";
+import { googleDrive } from "./client";
+import { Writable } from "stream";
 
 export interface ProviderConfig extends MapConfig {
-   api: GoogleConfig;
+  api: GoogleConfig;
 }
 
 class GoogleProvider extends MapProvider<ProviderConfig> {
-   track(postKey: string) {
-      return loadTrack(postKey);
-   }
+  track(postKey: string) {
+    return loadTrack(postKey);
+  }
 
-   gpx(postKey: string, stream: Writable) {
-      return streamGPX(postKey, stream);
-   }
+  gpx(postKey: string, stream: Writable) {
+    return streamGPX(postKey, stream);
+  }
 
-   authorizationURL() {
-      return Promise.resolve(googleDrive.client.authorizationURL);
-   }
+  authorizationURL() {
+    return Promise.resolve(googleDrive.client.authorizationURL);
+  }
 
-   async getAccessToken(req: IncomingMessage) {
-      if (req.url === undefined) {
-         throw 'Unable to get access token for missing URL';
-      }
-      const url = parse(req.url, true);
-      const code = unlist(url.query['code'], true);
-      return googleDrive.client.getAccessToken(code);
-   }
+  async getAccessToken(req: IncomingMessage) {
+    if (req.url === undefined) {
+      throw "Unable to get access token for missing URL";
+    }
+    const url = parse(req.url, true);
+    const code = unlist(url.query["code"], true);
+    return googleDrive.client.getAccessToken(code);
+  }
+
+  clearCache() {
+    return;
+  }
 }
 
 /**
